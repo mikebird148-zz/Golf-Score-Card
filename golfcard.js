013c67ba644ad9c5bf44fcb88750bb31123ebd1f
Mike Bird <mikebird148@gmail.com>
2016-11-13 22:59:02 -0700

    somewhat finished product to turn in

--- golfcard.js
+++ golfcard.js
@@ -25,11 +25,6 @@ function loadCourse(theid) {
         $("#courseaddress2").html(selectedCourse.course.city);
         $("#coursenumber").html(selectedCourse.course.phone);
         $("#coursewebsite").html("<a target='_blank' href='" + selectedCourse.course.website + "'>Visit our Website</a>");
-        // if (selectedCourse.course.website === true) {
-        //     $("#coursewebsite").html("<a target='_blank' href='" + selectedCourse.course.website + "'>Visit our Website</a>");
-        // } else {
-        //     $("#coursewebsite").remove();
-        // }
         for (var i = 0; i < (selectedCourse.course.holes[0].tee_boxes.length - 1); i++) {
             $("#teetypes").append("<option value='" + i + "'>" + selectedCourse.course.holes[0].tee_boxes[i].tee_type + "</option>");
         }
@@ -37,41 +32,44 @@ function loadCourse(theid) {
 }
 
 function begincard() {
+    $("#cardsetupbtn").click(function(){
+        $("#cardsetup").remove();
+    });
     var totalboxes = "";
+    var outboxes = "";
+    var inboxes = "";
     players = $("#playercount").val();
     $("#leftcard").html("");
     $("#scorecontainer").css("display", "flex");
     for (var i = 1; i <= players; i++) {
         $("#leftcard").append("<div id='playerlabel" + i +"' class='playerlabels'> Player " + i + " <span id='editicon' class='removeicon fa fa-pencil-square-o' aria-hidden='true' onclick='editname(" + i + ")'></span><span class='removeicon fa fa-trash-o' aria-hidden='true' onclick='removeplayer("+ i +")'></span></div>");
-        totalboxes += "<div class='playerstyles' id='playertotal" + i + "'></div>";
+        totalboxes += "<div class='playerstyles' id='playertotal" + i + "'>-</div>";
+        outboxes += "<div class='playerstyles' id='playerouttotal" + i + "'>-</div>";
+        inboxes += "<div class='playerstyles' id='playerintotal" + i + "'>-</div>";
     }
     for (var c = 1; c <= numberofholes; c++) {
         $("#rightcard").append("<div id='column" + c +"' class='holcol'><div class='colheader'>" + c + "</div></div>");
     }
     $(".modalbackground").fadeOut();
-    $("#rightcard").append("<div id='outcol' class='holcol'><div class='colheader'>out</div>" + totalboxes + "</div>");
-    $("#rightcard").append("<div id='incol' class='holcol'><div class='colheader'>in</div>" + totalboxes + "</div>");
-    $("#rightcard").append("<div id='strokescol' class='holcol'><div class='colheader'>strokes</div>" + totalboxes + "</div>");
+    $("#rightcard").append("<div id='outcol' class='holcol'><div class='colheader'>out</div>" + outboxes + "</div>");
+    $("#rightcard").append("<div id='incol' class='holcol'><div class='colheader'>in</div>" + inboxes + "</div>");
     $("#rightcard").append("<div id='scorecol' class='holcol'><div class='colheader'>score</div>" + totalboxes + "</div>");
     buildholes();
-    $("#cardsetupbtn").click(function(){
-        $("#cardsetup").remove();
-    });
 }
 
 function buildholes() {
     numberofholes = selectedCourse.course.holes.length;
     $("#contact").removeClass("empty");
     $("#container").removeClass("empty");
+    for (var s = 0; s < numberofholes; s++) {
+        $("#par-values").append("<div id='parvalues" + (s + 1) + "' class='parstyles'>" + selectedCourse.course.holes[s].tee_boxes[0].par + "</div>");
+        $("#yardage-values").append("<div class='yardagestyles'>" + selectedCourse.course.holes[s].tee_boxes[0].yards + "</div>");
+        $("#handicap-values").append("<div class='handicapstyles'>" + selectedCourse.course.holes[s].tee_boxes[0].hcp + "</div>");
+    }
     for (var p = 1; p <= players; p++) {
         for (var h = 1; h <= numberofholes; h++) {
             $("#column" + h).append("<input onkeyup='calculatescore(" + p + ")' class='holebox' type='text' id='player" + p + "hole" + h + "' />");
         }
-    }
-    for (var s = 0; s < numberofholes; s++) {
-        $("#par-values").append("<div id='parvalues" + s + "' class='parstyles'>" + selectedCourse.course.holes[s].tee_boxes[0].par + "</div>");
-        $("#yardage-values").append("<div class='yardagestyles'>" + selectedCourse.course.holes[s].tee_boxes[0].yards + "</div>");
-        $("#handicap-values").append("<div class='handicapstyles'>" + selectedCourse.course.holes[s].tee_boxes[0].hcp + "</div>");
     }
     parYardageHandicap();
 }
@@ -102,12 +100,9 @@ function parYardageHandicap() {
 // }
 
 function editname(golferid) {
-    $("#playerlabel" + golferid).replaceWith("<form id='playerlabel" + golferid + "'><input id='nametobe' type='text' name='yourname' placeholder='Enter your name.' /><input id='submitname' type='submit' value='submit' /></form>");
-    // var theirname = $("#nametobe").keyup(function(){
-    //
-    // });
+    $("#playerlabel" + golferid).replaceWith("<div id='playerlabel" + golferid + "'><form><input id='theirname' type='text' placeholder='Enter your name.' /><input id='submitname' type='submit' value='ok' /></form></div>");
     $("#submitname").click(function(){
-            $("#playerlabel" + golferid).replaceWith("<div id='playerlabel" + golferid + "' class='playerlabels'>" + theirname + "<span id='editicon' class='removeicon fa fa-pencil-square-o' aria-hidden='true' onclick='editname(" + golferid + ")'></span><span class='removeicon fa fa-trash-o' aria-hidden='true' onclick='removeplayer(" + golferid + ")'></span></div>");
+            $("#playerlabel" + golferid).replaceWith("<div id='playerlabel" + golferid + "' class='playerlabels'>" + $("#theirname").val() + "<span id='editicon' class='removeicon fa fa-pencil-square-o' aria-hidden='true' onclick='editname(" + golferid + ")'></span><span class='removeicon fa fa-trash-o' aria-hidden='true' onclick='removeplayer(" + golferid + ")'></span></div>");
         }
     );
 }
@@ -120,15 +115,22 @@ function removeplayer(theid) {
 }
 
 function calculatescore(playerid) {
-    var thetotal = 0;
-    for (var t = 1; t <= numberofholes; t++) {
-        thetotal += Number($("#player" + playerid + "hole" + t).val());
+        numberofholes = selectedCourse.course.holes.length;
+        var thetotal = 0;
+        var theout = 0;
+        var thein = 0;
+        for (var t = 1; t <= numberofholes; t++) {
+            thetotal += Number($("#player" + playerid + "hole" + t).val());
+        }
+        for (var t = 1; t <= numberofholes; t++) {
+        theout += Number($("#player" + playerid + "hole" + t).val());
     }
-    // for (var t = 1; t <= 9; t++) {
-    //     thetotal += Number($("#player" + playerid + "hole" + t).val());
-    // }
-    // for (var t = 10; t <= 18; t++) {
-    //     thetotal += Number($("#player" + playerid + "hole" + t).val());
-    // }
-    $("#playertotal" + playerid).html(thetotal);
+    if (numberofholes == 18) {
+        for (var t = 10; t <= numberofholes; t++) {
+            thein += Number($("#player" + playerid + "hole" + t).val());
+        }
+    }
+        $("#playertotal" + playerid).html(thetotal);
+        $("#playerouttotal" + playerid).html(theout);
+        $("#playerintotal" + playerid).html(thein);
 }
